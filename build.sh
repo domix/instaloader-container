e3ce22b11cfb99bcf2f3905c9725a4029ac451a2
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-04-04 16:59:50 -0500

    wip

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-docker build -t domix/instaloader:4.3 -t domix/instaloader:latest .
+#!/usr/bin/env bash
+
+VER=4.3.1  && \
+  docker build -t domix/instaloader:latest \
+    -t domix/instaloader:$VER . && \
+  git release $VER && \
+  docker push domix/instaloader:$VER && 
+    docker push domix/instaloader:latest
